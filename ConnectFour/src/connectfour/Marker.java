d07a38f367b5e12076402e10de258b5584982787
Thirsty Kitty <Thirsty Kitty@SKA-PC-ACER>
2015-01-23 18:34:30 -0700

    initial commit

---
+++ ConnectFour/src/connectfour/Marker.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectfour;
+
+/**
+ *
+ * @author Cheryl Allred
+ */
+public class Marker {
+    // instance variables
+    int row;
+    int column;
+    Player player;
+    
+    public Marker ();
+    
+    {
+        
+    }
+    
+    public void occupyLocation(Player player) {
+        this.player = player;
+    }
+    
+    public void displayMarker() {
+        if (this.player != null) {
+            System.out.println(this.player.marker);
+        }
+        else {
+            System.out.println(" ");
+        }
+    }
+            
+       
+    
+}
