f9cd8d3a63d40018c91e6948d9487fa9b58ac4a8
Kevin <Kevin@Main-PC>
2015-01-29 20:37:38 -0700

    1/29/2014 8:37pm

--- ConnectFour/src/connectfour/Player.java
+++ ConnectFour/src/connectfour/Player.java
@@ -22,24 +22,27 @@ public class Player {
     
     
     public void displayName(){
-        System.out.println("I have " + wins + "wins and " + losses + "losses.");
+        System.out.println("I have " + wins + "wins and " + losses + "losses.\r\n");
     }
     public String getWinningStatistics(long wins, long losses, long ties){
+        
+        char oneHundred = 100;
+        
         if (wins < 0){
             System.out.println("The number of wins must be "
-                    + "greater than or equal to zero.");
+                    + "greater than or equal to zero.\r\n");
             return "-1";
         }
         
         if (losses < 0){
             System.out.println("The number of losses must be "
-                    + "greater than or equal to zero.");
+                    + "greater than or equal to zero.\r\n");
             return "-1";
         }
  
         if (ties < 0){
             System.out.println("The number of ties must be "
-                    + "greater than or equal to zero.");
+                    + "greater than or equal to zero.\r\n");
             return "-1";
         } 
         
@@ -49,10 +52,10 @@ public class Player {
             return "0";
         }
         
-        double winRatio = wins/totalScore *100;
+        double winRatio = wins/totalScore * oneHundred;
         
         String playerStatistics = "You have" + wins + "wins, "+ losses+" losses"
-                    + " and " + ties + "ties. Your win ratio is " +winRatio;
+                    + " and " + ties + "ties. Your win ratio is " + winRatio + "r\n";
         
         return playerStatistics;
         }
