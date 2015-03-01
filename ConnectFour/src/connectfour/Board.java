77b65de9e0849ab6c05ce4a5b5b0edfc5e276c77
luispeck <luis.enrique.peck@gmail.com>
2015-02-28 21:48:15 -0700

    fixed some things

--- ConnectFour/src/connectfour/Board.java
+++ ConnectFour/src/connectfour/Board.java
@@ -5,18 +5,19 @@
  */
 package connectfour;
 import java.awt.Point;
+import javax.swing.table.AbstractTableModel;
 /**
  *
  * @author Erin
  */
-public class Board {
+public class Board extends AbstractTableModel {
     
-    int rowCount = 6;
-    int columnCount = 7;
+    public int rowCount = 6;
+    public int columnCount = 7;
     
     private String name;
-    private Point boardDimensions = new Point();
-    private Player[][] boardLocations;
+    public Point boardDimensions = new Point();
+    public Player[][] boardLocations;
     
    public Board() {
    } 
@@ -25,6 +26,44 @@ public class Board {
         this.boardDimensions.setLocation(noRows, noRows);
         this.boardLocations = new Player[noRows][noColumns];
     }
+    
+    public Point getBoardDimensions(){
+        return boardDimensions;
+    }
+    
+    public void setBoardDimensions(Point boardDimensions){
+        this.boardDimensions = boardDimensions;
+    }
+    
+    public Player[][] getBoardLocations(){
+        return boardLocations;
+    }
+    
+    public void setBoardLocations(Player[][] boardLocations){
+        this.boardLocations = boardLocations;
+    }
+    
+    @Override
+    public int getRowCount(){
+        return(int) this.boardDimensions.getX();
+    }
+    
+    @Override
+    public int getColumnCount(){
+        return(int) this.boardDimensions.getY();
+    }
+    
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex){
+        return boardLocations[rowIndex][columnIndex];
+    }
+    
+    public Player getPlayerAt(int row, int column){
+        return this.boardLocations[row][column];
+    }
+    
+    
+    
     public void clearTheBoard() {
         for (int i = 0; i< this.boardLocations.length; i++) {
                 Player[] rowlocations = this.boardLocations[i];
@@ -34,6 +73,18 @@ public class Board {
         }
     }
 
+    public boolean locationOccupied(Point location){
+        int row = location.x;
+        int column = location.y;
+        
+        if(this.boardLocations[row][column] != null){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
        public void occupyLocation(Player player, int row, int column) {
         Player playerAtLocation = this.boardLocations[row][column];
 
@@ -48,13 +99,52 @@ public class Board {
 
         public int row;
         public int column;
+        public String value;
         public Player player;
 
         Location() {
         }
+    
+    
+    int getRow(){
+        return row;
     }
 
-   public void displaySize() {
-       System.out.println("\n\tThe board is " + this.rowCount + " by " + this.columnCount + " in size.");
-   }
+    void setRow(int row){
+        this.row = row;
+    }
+    
+    int getColumn(){
+        return column;
+    }
+    
+    void setColumn(int column){
+        this.column = column;
+    }
+    
+    String getValue(){
+        return value;
+    }
+    
+    void setValue(){
+        this.value = value;
+    }
+    
+    Player getPlayer(){
+        return player;
+    }
+    
+    public void setPlayer(Player player){
+        this.player = player;
+    }
+    
+    String[] getCoordinates(){
+        String[] coordinates = new String[2];
+        Integer intRow = this.getRow() + 1;
+        Integer intColumn = this.getColumn() + 1;
+        coordinates[0] = intRow.toString();
+        coordinates[1] = intColumn.toString();
+        return coordinates;
+    }
+    }
 }
--- ConnectFour/src/connectfour/BoardView.java
+++ ConnectFour/src/connectfour/BoardView.java
@@ -20,13 +20,24 @@ public class BoardView {
         this.printDividerRow();
         for (int i = 0;i < this.board.getRowCount();i++){
             Player[] rowOfLocations = this.board.getBoardLocations()[i];
-            this.PrintRow(i+1, rowOfLocations);
+            this.printRow(i+1, rowOfLocations);
             this.printDividerRow();
         }
         System.out.println();
          
         return null;
     }
+private void printHeadRow(){
+    
+    System.out.print("\n\t     1  ");
+    int columnsInRow = this.board.getColumnCount();
+    
+    for (int i = 1;i < columnsInRow -1;i++){
+        int col = i + 1;
+        System.out.print("  " +col+ "  ");
+    }
+    System.out.print(" "+ columnsInRow + " ");
+}
 private void printDividerRow() {
 
         // divider for first column
@@ -40,6 +51,24 @@ private void printDividerRow() {
         // divider for last row
         System.out.print("━━|");
     }
+private void printRow(int rowNumber,Player[] rowLocations){
+    
+    String letter = " ";
+    if (rowLocations[0] != null){
+        letter = rowLocations[0].marker;
+    }
+    System.out.print("\n\t" + rowNumber + " | " + letter + "  |");
+    
+
+    for (int i =1; i< rowLocations.length;i++){
+         if(rowLocations[i] == null){
+             letter = " ";
+         } else {
+             letter = rowLocations[i].marker;
+         }
+         System.out.print(" " + letter + " |");
+         }
+}
     
 
 }
