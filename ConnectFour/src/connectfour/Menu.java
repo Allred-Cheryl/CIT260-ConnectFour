77c03de985eee9fec6cec610d5c49b9acc1e4a81
Erin <Erin@Leo>
2015-03-12 16:34:27 -0400

    Menu super-class

--- ConnectFour/src/connectfour/Menu.java
+++ ConnectFour/src/connectfour/Menu.java
@@ -5,7 +5,7 @@
  */
 package connectfour;
 
-import java.util.Scanner
+import java.util.Scanner;
 /**
  *
  * @author Erin
@@ -22,7 +22,54 @@ public abstract class Menu {
         
     }
     
-//to be continued...I just wanted to get the Menu super-class going
+//please edit as necessary
 
+public abstract String executeCommands(Object object);
 
+public String[][] getMenuItems() {
+    return menuItems;
+}
+
+public void setMenuItems(String[][] menuItems) {
+    this.menuItems = menuItems;
+}
+
+public final void display() {
+    System.out.println("\n\t=============================================================");
+    System.out.println("\tEnter the letter associated with one of the following commands:");
+
+    for (int i = 0; i < this.menuItems.length; i++) {
+        System.out.println("\t  " + menuItems[i][0] + "\t" + menuItems[i][1]);
+    }
+    System.out.println("\t================================================================");
+}
+
+private boolean validCommand(String command) {
+    String[][] items = this.menuItems;
+    
+    for (String[] item : this.menuItems) {
+        if (item[0].equals(command)) {
+            return true;
+        }
+    }
+    return false;
+}
+protected final String getCommand() {
+
+        Scanner inFile = ConnectFour.getInputFile();
+        String command;
+        boolean valid = false;
+        do {
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            valid = validCommand(command);
+            if (!validCommand(command)) {
+                new ConnectFourError().displayError("Invalid command. Please enter a valid command.");
+                continue;
+            }
+                
+        } while (!valid);
+        
+        return command;
+    }
 }
